14c35b0ea714cbf46bf082f3fa772057e7649bc7
Minghai Qin <minghai.qin@cocopie.ai>
2022-04-13 16:41:50 -0700

    first commit

---
+++ scripts/start_docker.sh
@@ -0,0 +1,7 @@
+
+
+DOCKER_IMAGE=${1:-'transformer'}
+DATA_DIR=${2:-'/nasmnt/minghai.qin/PyTorch_examples/DeepLearningExamples/PyTorch/Translation/Transformer/'}
+EXTRA=${3:-" "}
+
+nvidia-docker run -it --rm --ipc=host -v ${DATA_DIR}/data/wmt14_en_de_joined_dict:/data/wmt14_en_de_joined_dict -v ${PWD}:/home -w /home -v ${PWD}/results:/results ${EXTRA} ${DOCKER_IMAGE}
