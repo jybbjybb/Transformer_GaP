ed0fa1e08ea7ec996687fbcd9897381e0b43437b
jybb <qinminghai@gmail.com>
2022-04-13 17:05:16 -0700

    Update start_docker.sh

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -1,7 +1,7 @@
 
 
 DOCKER_IMAGE=${1:-'transformer'}
-DATA_DIR=${2:-'/nasmnt/minghai.qin/PyTorch_examples/DeepLearningExamples/PyTorch/Translation/Transformer/'}
+DATA_DIR=${2:-'.'}
 EXTRA=${3:-" "}
 
 nvidia-docker run -it --rm --ipc=host -v ${DATA_DIR}/data/wmt14_en_de_joined_dict:/data/wmt14_en_de_joined_dict -v ${PWD}:/home -w /home -v ${PWD}/results:/results ${EXTRA} ${DOCKER_IMAGE}
